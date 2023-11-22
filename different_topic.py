4336a641f2b0cb54722a48fd5e195be63e502fc1
TomaszSlupik <slupiktomasz@gmail.com>
2023-11-22 08:41:00 +0100

    change

--- different_topic.py
+++ different_topic.py
@@ -77,4 +77,71 @@ print(note2.content())
 print ("Data utworzenia notatek:", note1.getcreation_time())
 print('---')
 
-# ćw 5 
+# ćw 5 Sprawdzenie słowa 'Python' - wielkość liter ma znaczenie
+class Checkword ():
+    def __init__(self, word):
+        self.word = word
+
+    def find(self, findWord):
+        return findWord in self.word
+    
+checkOne = Checkword('Object Oriented Programming in Python.')
+
+print(checkOne.find('python'))
+print(checkOne.find('Python'))
+print('---')
+
+# ćw 6 wielkość liter nie ma znaczenie
+class CheckLower ():
+    def __init__(self, word):
+        self.word = word
+
+    def find(self, find):
+        return find.lower() in self.word.lower()
+    
+checkTwo = CheckLower('Object Oriented Programming in Python.')
+print (checkTwo.find('python'))
+print (checkTwo.find('Python'))
+print('---')
+
+# ćw 7 Notebook - dodanie do listy notatek
+class Notebook ():
+    def __init__(self):
+        self.notes = []
+
+    def new_note(self, myNote):
+        self.notes.append(myNote)
+    
+    def __repr__(self):
+        return f"Note(content={self.notes})"
+    
+notebook =  Notebook()  
+
+
+notebook.new_note('My first note.')
+notebook.new_note('My second note.')
+
+print (notebook.notes)
+print('---')
+
+# ćw 8 Wiele argumentów *args
+class Date ():
+    def __init__(self, *specialize):
+        self.specialize = specialize
+    
+    def search(self):
+        return f"{self.specialize}"
+    
+date = Date('Big Data', 'Data Science', 'Machine Learning')
+
+print(date.search())
+print('---')
+
+
+# ćw 9 Unpacking - rozpakowywanie 
+number = (45, 23, 14)
+a, b, c = number
+
+x = number[0]
+print (f"Szybszy sposób: {a}, lub tradycyjny sposób: {x}")
+print('---')
